111c5db418f832f3e1e45148fae457981ba72c90
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-26 22:44:18 +0530

    Created a spinner loader

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Loader from "./components/Loader";
+
+const url = "https://course-api.com/react-tours-project";
+
+export default function App() {
+  return (
+    <div className="container mx-auto">
+      <Loader />
+    </div>
+  );
+}
